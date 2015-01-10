f7f3db959ec16cc7ccdda44f6719593cb8ba7001
Joe Burdick <josephdburdick@gmail.com>
2015-01-10 14:33:23 -0500

    Create task, list, and task manager modules using exports.

---
+++ src/task.js
@@ -0,0 +1,11 @@
+define(function(){
+	'use strict';
+	function Task(){
+		this.completed = false;
+		this.name = name
+	}
+	Task.prototype = {};
+	Task.prototype.constructor = Task;
+
+	return Task;
+});
